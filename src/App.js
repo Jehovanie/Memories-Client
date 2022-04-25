2632b275252d85ab87ecea247a0c33dcfbff9b94
Jehovanie <jehovanieram@gmail.com>
2022-04-26 00:12:04 +0300

    add styles and import then in each component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Container, AppBar, Grow, Grid, Typography } from "@material-ui/core";
+
+import memories from "./images/memorise.jpg";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+import useStyles from './styles';
 
 const App = () => {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>App </h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">
+                    Memories
+                </Typography>
+                <img className={classes.image} src={memories} alt="Memories" width="60" align="center" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItem="stretch" spacing={4}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 };
 
---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import useStyles from './style';
+
+const Post = () => {
+
+    const classes = useStyles();
+    return (
+        <div>
+            POST
+        </div>
+    )
+}
+
+export default Post;
