f1dfffc51569dab0844a8cd7f147269d457e211a
Jehovanie <jehovanieram@gmail.com>
2022-04-29 14:19:22 +0300

    change the instance store createStore to configureStore

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+// import { createStore, applyMiddleware, compose } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import reducer from './reducers';
 
 import App from './App';
 
-const store = createStore(reducer, compose(applyMiddleware(thunk)));
+// const store = createStore(reducer, compose(applyMiddleware(thunk)));
+const store = configureStore({ reducer: reducer })
 
 ReactDOM.render(
     <Provider store={store}>
