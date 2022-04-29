20f6c2b58f9df28393b9b134e75a55d791d46c7f
Jehovanie <jehovanieram@gmail.com>
2022-04-29 19:36:20 +0300

    change the font size of the title to h3

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">
+                <Typography className={classes.heading} variant="h3" align="center">
                     Memories
                 </Typography>
                 <img className={classes.image} src={memories} alt="Memories" width="60" align="center" />
