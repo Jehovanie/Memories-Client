ea05d93fa1ee20d72dfa9d2f29cf6e540a123da4
Jehovanie <jehovanieram@gmail.com>
2022-05-04 09:00:48 +0300

    add directory constant to keep all type of the action

--- src/actions/posts_actions.js
+++ src/actions/posts_actions.js
@@ -1,14 +1,14 @@
+import { FETCH_ALL, DELETE, CREATE, UPDATE, ADD_LIKE } from "../constants/actionType";
 import * as api from "../api";
 
 // Action Creators
 export const getPosts = () => async (dispatch) => {
 
     // const action = { type: 'FETCH_ALL', payload: [] }
-
     try {
 
         const { data } = await api.fetchPosts();
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
 
         console.log("ERROR FETCH_ALL : " + error.message);
@@ -21,7 +21,7 @@ export const createPost = (post) => async (dispatch) => {
     try {
 
         const { data } = await api.createPost(post);
-        dispatch({ type: 'CREATE', payload: data })
+        dispatch({ type: CREATE, payload: data })
     } catch (error) {
 
         console.log("ERROR CREATE: " + error.message);
@@ -33,7 +33,7 @@ export const updatedPost = (id, post) => async (dispatch) => {
     try {
 
         const { data } = await api.updatedPost(id, post);
-        dispatch({ type: 'UPDATE', payload: data })
+        dispatch({ type: UPDATE, payload: data })
     } catch (error) {
         console.log("ERROR UPDATE : " + error.message);
     }
@@ -43,7 +43,7 @@ export const delete_post = (id) => async (dispatch) => {
 
     try {
         await api.deletedPost(id);
-        dispatch({ type: 'DELETE', payload: id });
+        dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log("ERROR DELETE : " + error)
     }
@@ -53,7 +53,7 @@ export const add_like_post = (id) => async (dispatch) => {
 
     try {
         const { data } = await api.add_like(id);
-        dispatch({ type: 'ADD_LIKE', payload: data })
+        dispatch({ type: ADD_LIKE, payload: data })
     } catch (error) {
         console.log("ERROR ADD LIKE :" + error.message)
     }
