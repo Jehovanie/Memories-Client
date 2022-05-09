7dc5f9cd36051fc50f85c9a8713e51f941179c1f
Jehovanie <jehovanieram@gmail.com>
2022-05-09 09:48:18 +0300

    push all

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container } from "@material-ui/core"
+import useStyles from "./styles"
+import LockOutlinedIcon from "@material-ui/icons/LockOpenOutlined"
 
 const Auth = () => {
     const state = null;
+    const classes = useStyles();
+    const isSignup = false;
+
+    const handleSubmit = () => { }
     return (
-        <div>
-            Auth working
-        </div>
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5">
+                    {isSignup ? "Sign Up" : "Sign In"}
+                </Typography>
+
+                <form className={classes.form} onSubmit={handleSubmit}>
+
+                </form>
+            </Paper>
+
+        </Container>
     );
 };
 
